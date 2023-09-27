7c078271e2c5e2a204289070dd7cb6b45867cad1
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-09-27 18:54:48 +0200

    Hamburger menu on main page v1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,3 +4,10 @@ document.querySelector("#currentYear").innerHTML = `©${year}`;
 document.querySelector(
   "#lastModified"
 ).innerHTML = `Last Modification: ${lastModified}`;
+
+/*--------------------------------------------------*/
+
+document.querySelector('#burger').addEventListener('click', () => {
+	document.querySelector('.navigation').classList.toggle('open');
+	document.querySelector('#burger').classList.toggle('open');
+});
