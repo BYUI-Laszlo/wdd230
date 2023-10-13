2757bfa149c35df853269ce57b6c6746f0ef8cbd
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-10-13 20:20:05 +0200

    Dark Mode

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -5,3 +5,12 @@ document.querySelector('#burger').addEventListener('click', () => {
 	document.querySelector('.navigation').classList.toggle('open');
 	document.querySelector('#burger').classList.toggle('open');
 });
+
+document.querySelector("#dark-btn").addEventListener("click", () => {
+	document.querySelector("#dark-btn").classList.toggle("dark");
+	document.querySelector(".events").classList.toggle("dark");
+	document.querySelector(".spotlight").classList.toggle("dark");
+	document.querySelector(".weather").classList.toggle("dark");
+	document.querySelector("body").classList.toggle("dark");
+	document.querySelector("nav").classList.toggle("dark");
+});
