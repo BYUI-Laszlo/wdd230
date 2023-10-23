e403e61b53e25ddf1efb7ed02686ae6d3bd03997
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-10-23 15:17:13 +0200

    localstorage

---
+++ lesson07/scripts/bom.js
@@ -0,0 +1,48 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+button.addEventListener("click", () => {
+    if (input.value != ""){
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = "";
+        input.focus();
+    };
+});
+
+function displayList(item) {
+    const listItm = document.createElement("li");
+        const listBtn = document.createElement("button");
+        listItm.textContent = input.value;
+        listBtn.textContent = "❌";
+        listBtn.ariaLabel = "Delete";
+        listItm.append(listBtn);
+        list.append(listItm);
+        listBtn.addEventListener("click", () => {
+            list.removeChild(listItm);
+            deleteChapter(listItm.textContent)
+            input.focus();
+        });
+}
+
+
+let chaptersArray = getChapterList() || [];
+chaptersArray.array.forEach(chapter => {
+    displayList(chapter);
+});
+
+function setChapterList() {
+  localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
