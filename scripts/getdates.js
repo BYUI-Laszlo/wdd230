5808cc33c7904cddf2a3a9fbea9529eb4e287dac
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-10-24 21:20:25 +0200

    number of visits

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -11,3 +11,15 @@ document.querySelector('#burger').addEventListener('click', () => {
 	document.querySelector('.navigation').classList.toggle('open');
 	document.querySelector('#burger').classList.toggle('open');
 });
+
+/*--------------------------------------------------*/
+
+let visitNumber = Number(localStorage.getItem("visitStored")) || 0;
+if (visitNumber == 0){
+	document.querySelector(".visits").innerHTML = "This is your first time on this page";
+}
+else {
+	document.querySelector(".visits").innerHTML = "Number of visits: "+ visitNumber;
+}
+visitNumber++;
+localStorage.setItem("visitStored", visitNumber);
