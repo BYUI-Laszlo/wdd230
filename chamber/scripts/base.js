bf49e9d96910aadbe21c76fa8d80735cab3cd987
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-11-02 09:32:00 +0100

    main join button and darkswitch consinstency

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -1,3 +1,24 @@
+let darkSwitch = localStorage.getItem("dark-switch") || "";
+
+document.addEventListener("DOMContentLoaded", darktest);
+
+function darktest(){
+	if (darkSwitch == "dark") {
+		document.querySelector("#dark-btn").classList.add("dark");
+		document.querySelector("body").classList.add("dark");
+	}
+}
+function darkAddRemove () {
+	if (darkSwitch == "dark") {
+		localStorage.removeItem("dark-switch", "dark");
+		darkSwitch = "";
+	}
+	else {
+		localStorage.setItem("dark-switch", "dark");
+		darkSwitch = "dark";
+	}
+}
+
 let lastModified = new Date(document.lastModified);
 document.querySelector("#lastModified").innerHTML = `Last Modification: ${lastModified}`;
 
@@ -7,6 +28,7 @@ document.querySelector('#burger').addEventListener('click', () => {
 });
 
 document.querySelector("#dark-btn").addEventListener("click", () => {
+	darkAddRemove ();
 	document.querySelector("#dark-btn").classList.toggle("dark");
 	document.querySelector("body").classList.toggle("dark");
 });
