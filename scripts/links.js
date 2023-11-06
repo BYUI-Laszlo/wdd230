993d2e802247a98bfcd65d1a9c7643bbeff4f759
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-11-06 23:45:33 +0100

    JSON Data driven lessons list

---
+++ scripts/links.js
@@ -0,0 +1,37 @@
+const baseURL = "https://byui-laszlo.github.io/wdd230/";
+const linksURL = "https://byui-laszlo.github.io/wdd230/data/links.json";
+const card = document.querySelector(".card");
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data.lessons);
+    console.log(data);
+}
+
+const displayLinks = (lessons) => {
+    let sectonTitle = document.createElement("h3");
+    sectonTitle.textContent = `Learning Activities`;
+    card.appendChild(sectonTitle);
+    let list = document.createElement("ul");
+    card.appendChild(list);
+
+    lessons.forEach((week) => {
+        let listElement = document.createElement("li");
+        let weekNumber = document.createElement("span");
+        weekNumber.textContent = `W${week.lesson}`;
+        listElement.appendChild(weekNumber);
+
+        week.links.forEach((click) => {
+            let anchor = document.createElement("a");
+            anchor.setAttribute("href", click.url);
+            anchor.setAttribute("target", "_blank");
+            anchor.textContent = ` | ${click.title}`;
+            listElement.appendChild(anchor); 
+        });
+
+        list.appendChild(listElement);
+    });
+}
+
+getLinks();
