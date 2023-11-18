7b29c842995d99ccbbfa5c56c001c0b6770a772c
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-11-18 12:18:24 +0100

    Prophets additional data

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -12,14 +12,20 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement("section");
         let fullName = document.createElement("h2");
+        let dateOfBirth = document.createElement("p");
+        let placeOfBirth = document.createElement("p");
         let portrait = document.createElement("img");
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        dateOfBirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+        placeOfBirth.textContent = `Place of Birth: ${prophet.birthplace}`;
         portrait.setAttribute("src", prophet.imageurl);
         portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute("loading", "lazy");
         portrait.setAttribute("width", "340");
         portrait.setAttribute("height", "440");
         card.appendChild(fullName);
+        card.appendChild(dateOfBirth);
+        card.appendChild(placeOfBirth);  
         card.appendChild(portrait);
         cards.appendChild(card);
     });
