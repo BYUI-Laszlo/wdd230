98152749985837c965c75a1441040a1da06d6c12
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-12-04 07:45:09 +0100

    Scoots header footer

---
+++ scoots/scripts/base.js
@@ -0,0 +1,31 @@
+document.querySelector('#burger').addEventListener('click', () => {
+	document.querySelector('.navigation').classList.toggle('open');
+	document.querySelector('#burger').classList.toggle('open');
+});
+
+let darkSwitch = localStorage.getItem("dark-switch") || "";
+
+document.addEventListener("DOMContentLoaded", darktest);
+
+function darktest(){
+	if (darkSwitch == "dark") {
+		document.querySelector("#dark-btn").classList.add("dark");
+		document.querySelector("body").classList.add("dark");
+	}
+}
+function darkAddRemove () {
+	if (darkSwitch == "dark") {
+		localStorage.removeItem("dark-switch", "dark");
+		darkSwitch = "";
+	}
+	else {
+		localStorage.setItem("dark-switch", "dark");
+		darkSwitch = "dark";
+	}
+}
+
+document.querySelector("#dark-btn").addEventListener("click", () => {
+	darkAddRemove ();
+	document.querySelector("#dark-btn").classList.toggle("dark");
+	document.querySelector("body").classList.toggle("dark");
+});
