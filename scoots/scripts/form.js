514a966541f6c7c78edcc6e4863f077207a175f1
BYUI-Laszlo <laszlo.tem@gmail.com>
2023-12-05 23:50:52 +0100

    reservations page

---
+++ scoots/scripts/form.js
@@ -0,0 +1,55 @@
+// Timestamp
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth();
+const day = currentDate.getDate();
+const hour = currentDate.getHours();
+const minute = currentDate.getMinutes();
+
+const timeStamp = `${month}/${day}/${year} ${hour}:${minute}`
+
+document.querySelector("#timeStamp").value = timeStamp;
+
+// Radiobutton
+function raidoList() {
+    const radio = document.getElementById("fullRadio");
+    const dayList = document.getElementById("fullNumber");
+
+    if (radio.checked) {
+        dayList.style.display = "inline-block";
+    } else {
+        dayList.style.display = "none";
+    }
+}
+
+// Checkbox
+function toggleDropdown(scooterType) {
+        const checkbox = document.getElementById(`${scooterType}Checkbox`);
+        const dropdown = document.getElementById(`${scooterType}Number`);
+
+        if (checkbox.checked) {
+            dropdown.style.display = 'inline-block';
+        } else {
+            dropdown.style.display = 'none';
+        }
+    }
+// At least 1 checkbox selected
+document.addEventListener('DOMContentLoaded', function () {
+        const form = document.querySelector('form');
+
+        form.addEventListener('submit', function (event) {
+            const checkboxes = document.querySelectorAll('.vehicleCheckbox');
+            let atLeastOneChecked = false;
+
+            checkboxes.forEach(function (checkbox) {
+                if (checkbox.checked) {
+                    atLeastOneChecked = true;
+                }
+            });
+
+            if (!atLeastOneChecked) {
+                alert('Please select at least one vehicle.');
+                event.preventDefault(); // Prevent form submission
+            }
+        });
+    });
